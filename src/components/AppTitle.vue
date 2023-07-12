b0d156f70d21d95f24999591556b73d0cdb455a6
stiva98 <samustiva9810@gmail.com>
2023-07-12 17:33:28 +0200

    ADD title e esercizio base terminato

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+
+  props: {
+    title: String,
+  }
+
+}
+
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: brown;
+  text-align: center;
+  margin: 0;
+}
+</style>
