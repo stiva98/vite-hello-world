66bfbc62a8ba1faa3392da4252427b13cce3c07c
stiva98 <samustiva9810@gmail.com>
2023-07-12 21:11:25 +0200

    EDIT in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
-<script setup>
+<script>
 import AppTitle from './components/AppTitle.vue'
 import AppHero from './components/AppHero.vue';
+
+export default {
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
 </script>
 
 <template>
